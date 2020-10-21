394847d686dc4de071bd9afba649c9e642e80829
etienne1698 <etienne.robert1698@gmail.com>
2020-10-21 14:19:22 +0200

    up (couleur description en blanc)

--- views/description.php
+++ views/description.php
@@ -1,11 +1,11 @@
-<section class="section zone-is-override has-background-primary has-text-centered">
+<section class="section zone-is-override has-background-primary has-text-centered has-text-white">
     <div class="container">
         <div class="columns is-centered">
             <div class="column is-three-fifths">
-                <h1 class="title is-spaced is-size-3-desktop is-size-4-mobile"
+                <h1 class="title is-spaced is-size-3-desktop is-size-4-mobile has-text-white"
                     data-sal="zoom-in"
                     data-sal-delay="50">Etienne ROBERT</h1>
-                <h2 class="subtitle is-size-5-desktop"
+                <h2 class="subtitle is-size-5-desktop has-text-white"
                     data-sal="zoom-in"
                     data-sal-delay="50">
                     <?php echo Context::getContext()->translate('description'); ?>
